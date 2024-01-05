7ee660ee462b9ff2875375616989c73b5a6b1a56
Arturs Razmuss <arturs.razmuss@gmail.com>
2024-01-05 17:17:06 +0200

    create observer-ui-client draft

---
+++ observer-ui-client/src/main/java/com/arpc/crypto/order/boundary/OrderBookResource.java
@@ -0,0 +1,35 @@
+package com.arpc.crypto.order.boundary;
+
+import com.arpc.crypto.order.OrderBookQueryService;
+import com.arpc.crypto.order.OrderSpreadRequest;
+import io.quarkus.grpc.GrpcClient;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.time.Instant;
+
+@Path("/hello")
+public class OrderBookResource {
+
+    @GrpcClient("orderBookQueryService")
+    OrderBookQueryService orderBookQueryService;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        orderBookQueryService.getBestPrices(OrderSpreadRequest.newBuilder()
+                .setSymbol("BTC")
+                .setStartTimestamp(TimestampConverter.convertToTimestamp(Instant.now().minusSeconds(60)))
+                .setEndTimestamp(TimestampConverter.convertToTimestamp(Instant.now()))
+                .build())
+                .subscribe().with(orderSpreadResponse -> {
+            System.out.println("Best ask price: " + orderSpreadResponse.getBestAskPrice());
+            System.out.println("Best bid price: " + orderSpreadResponse.getBestBidPrice());
+        }, throwable -> {
+            System.out.println("Error: " + throwable.getMessage());
+        });
+        return "Hello from RESTEasy Reactive";
+    }
+}
